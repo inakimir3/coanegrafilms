0c7978b9f30ea84c8d7f844ee54a7a2499df6ed1
Iñaki Mir <imirbelloso@gmail.com>
2026-03-15 18:51:57 +0100

    projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,27 +1,60 @@
-export interface Project {
-  id: string;
-  title: string;
-  video: string;
-  link: string;
-}
 
-export const projects: Project[] = [
+
+ const rawProjects = [
   { 
-    id: '01', 
-    title: 'Zalando', 
-    video: '/videos/test1.mp4', 
-    link: '/work/zalando' 
-  },
-  { 
-    id: '02', 
     title: 'Vi de la Terra', 
-    video: '/videos/test2.mp4', 
-    link: '/work/vi-de-la-terra' 
+    video: '/videos/test1.mp4', 
+    link: '/work/videlaterra', 
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    gallery: [
+      '/images/projects/videlaterra/videlaterra01.webp',
+      '/images/projects/videlaterra/videlaterra02.webp',
+      '/images/projects/videlaterra/videlaterra03.webp',
+      '/images/projects/videlaterra/videlaterra04.webp',
+      '/images/projects/videlaterra/videlaterra05.webp',
+      '/images/projects/videlaterra/videlaterra01.webp',
+      '/images/projects/videlaterra/videlaterra02.webp',
+      '/images/projects/videlaterra/videlaterra03.webp',
+      '/images/projects/videlaterra/videlaterra04.webp',
+      '/images/projects/videlaterra/videlaterra05.webp',
+      '/images/projects/videlaterra/videlaterra01.webp',
+      '/images/projects/videlaterra/videlaterra02.webp',
+      '/images/projects/videlaterra/videlaterra03.webp',
+      '/images/projects/videlaterra/videlaterra04.webp',
+      '/images/projects/videlaterra/videlaterra05.webp',
+    ]
   },
   { 
-    id: '03', 
+    title: 'Zalando', 
+    video: '/videos/test2.mp4', 
+    link: '/work/zalando',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  { 
     title: 'Ahren', 
     video: '/videos/video-home.mp4', 
     link: '/work/ahren' 
   },
+  { 
+    title: 'Zalando2', 
+    video: '/videos/test1.mp4', 
+    link: '/work/zalando2',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  { 
+    title: 'Vi de la Terra2', 
+    video: '/videos/test2.mp4', 
+    link: '/work/vi-de-la-terra2',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  { 
+    title: 'Ahren2', 
+    video: '/videos/video-home.mp4', 
+    link: '/work/ahren2' 
+  },
 ];
+
+export const projects = rawProjects.map((project, index) => ({
+  ...project,
+  id: (index + 1).toString().padStart(2, '0')
+}));
