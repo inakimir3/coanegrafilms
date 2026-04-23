a9f2a94ee9dcdf6775f2089f520c13c39793b2c6
Iñaki Mir <imirbelloso@gmail.com>
2026-04-23 18:53:15 +0200

    new work

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,6 +6,7 @@
     video: '/videos/test1.mp4', 
     link: '/work/videlaterra', 
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    cover: '/images/projects/videlaterra/videlaterra05.webp',
     gallery: [
       '/images/projects/videlaterra/videlaterra01.webp',
       '/images/projects/videlaterra/videlaterra02.webp',
@@ -28,29 +29,35 @@
     title: 'Zalando', 
     video: '/videos/test2.mp4', 
     link: '/work/zalando',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    cover: '/images/projects/videlaterra/videlaterra01.webp',
   },
   { 
     title: 'Ahren', 
     video: '/videos/video-home.mp4', 
-    link: '/work/ahren' 
+    link: '/work/ahren',
+    cover: '/images/projects/videlaterra/videlaterra03.webp',
   },
   { 
     title: 'Zalando2', 
     video: '/videos/test1.mp4', 
     link: '/work/zalando2',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    cover: '/images/projects/videlaterra/videlaterra02.webp',
   },
   { 
     title: 'Vi de la Terra2', 
     video: '/videos/test2.mp4', 
     link: '/work/vi-de-la-terra2',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    cover: '/images/projects/videlaterra/videlaterra05.webp',
+
   },
   { 
     title: 'Ahren2', 
     video: '/videos/video-home.mp4', 
-    link: '/work/ahren2' 
+    link: '/work/ahren2',
+    cover: '/images/projects/videlaterra/videlaterra04.webp',
   },
 ];
 
